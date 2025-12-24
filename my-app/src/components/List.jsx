af2e64d5f7da190ced486f2a026de3bf898497c7
imenebaraitame <baraitame.imene@gmail.com>
2025-12-24 16:16:16 +0100

    feat: edit task

--- my-app/src/components/List.jsx
+++ my-app/src/components/List.jsx
@@ -3,15 +3,28 @@ import {useState} from 'react';
 function List(){
     const [inputTask, setInputTask] = useState("");
     const [tasks, setTasks] = useState([]);
+    const [editingId, setEditingId] = useState(null);
 
     const handleTnputChange = (e) => {
         setInputTask(e.target.value);
     };  
     const addTask = ()=> {
-        setTasks(prev => [
-            ...prev,
-            { id: Date.now(), text: inputTask }
-        ]);
+        if(!inputTask.trim()) return;
+
+        if(editingId === null) {
+            setTasks(prev => [
+                ...prev,
+                { id: Date.now(), text: inputTask }
+            ]);
+        } else {
+            setTasks(prev =>
+                prev.map(task =>
+                    task.id === editingId ? {...task, text:inputTask} : task
+                )
+            );
+            setEditingId(null);
+        }
+        
         setInputTask("");
     };
     const deleteTask = (id) => {
@@ -19,12 +32,18 @@ function List(){
             prev.filter(task =>task.id !== id)
         );
     }
+    const editTask = (task) => {
+        setInputTask(task.text || "");
+        setEditingId(task.id);
+    }
     
     return(
         <>
             <div className="input-container">
-                <input id="input-task" type="text" value={inputTask} onChange={handleTnputChange}/>
-                <button className="btn-add" onClick={addTask}>Add</button>
+                <input id="input-task" type="text" value={inputTask} onChange={handleTnputChange} placeholder="Enter a task..."/>
+                <button className="btn-add" onClick={addTask}>
+                    {editingId === null ? "Add" : "Update"}
+                </button>
             </div>
             <div className="list-container">
                 <div className="task-container">
@@ -34,6 +53,7 @@ function List(){
                                 <label htmlFor="note" >{task.text}</label>
                                 <input type="checkbox" id="note"/>
                                 <button onClick={() => deleteTask(task.id)}>Delete</button>
+                                <button onClick={() => editTask(task)}>Edit</button>
                             </div>    
                            
                         ))
