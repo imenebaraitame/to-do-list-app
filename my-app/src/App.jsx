ee069836a4559ed40452cc630533c000947c6b57
imenebaraitame <baraitame.imene@gmail.com>
2025-12-24 23:14:09 +0100

    feat: implement dark and light mode toogle

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,21 @@
 import List from './components/list';
+// import { useState } from 'react';
 
 function App() {
-
-
+  // const [darkMode, setDarkMode] = useState(false);
+  //  const toggleMode = () => {
+  //       setDarkMode(prev => !prev);
+  //   };
+  
   return (
     <>
-      <List/>
+      {/* <div className={darkMode ? "dark" : "light"}>
+          <button className="toggle-btn" onClick={toggleMode}>
+              <i className={darkMode ? "fa-solid fa-sun" : "fa-solid fa-moon"}></i>
+          </button> */}
+      
+        <List />  
+      {/* </div>  */}
     </>
   );
 }
--- my-app/src/components/List.jsx
+++ my-app/src/components/List.jsx
@@ -4,6 +4,7 @@ function List(){
     const [inputTask, setInputTask] = useState("");
     const [tasks, setTasks] = useState([]);
     const [editingId, setEditingId] = useState(null);
+    const [darkMode, setDarkMode] = useState(false);
 
     const handleTnputChange = (e) => {
         setInputTask(e.target.value);
@@ -46,37 +47,47 @@ function List(){
         );
     };
     
+    const toggleMode = () => {
+        setDarkMode(prev => !prev);
+    };
+    
     return(
         <>
-            <div className="todo-wrapper">
-                <h1 className='title'>TODO LIST</h1>
-                <div className="input-container">
-                    <input type="text" value={inputTask} onChange={handleTnputChange} placeholder="Enter a task..."/>
-                    <button className="btn-add" onClick={addTask}>
-                        {editingId === null ? "Add" : "Update"}
-                    </button>
-                </div>
-            
-                <div className="task-container">
-                    {
-                        tasks.map(task => (
-                            <div key={task.id} className="task-item">
+            <div className={darkMode ? "dark" : "light"}>
+                <div className="todo-wrapper">
+                    <h1 className='title'>TODO LIST</h1>
+                    <div className="input-container">
+                        <input type="text" value={inputTask} onChange={handleTnputChange} placeholder="Enter a task..."/>
+                        <button className="btn-add" onClick={addTask}>
+                            {editingId === null ? "Add" : "Update"}
+                        </button>
+                        
+                        <button className="toggle-btn" onClick={toggleMode}>
+                            <i className={darkMode ? "fa-solid fa-sun" : "fa-solid fa-moon"}></i>
+                        </button>
+                    </div>
+                
+                    <div className="task-container">
+                        {
+                            tasks.map(task => (
+                                <div key={task.id} className="task-item">
+                                    
+                                    <div className="checkbox-wrapper-1">
+                                        <input id="checkbox-filled-1" className="checkbox-1" type="checkbox" checked={task.completed} onChange={() => toggleCompleted(task.id)}/>
+                                        <label htmlFor="checkbox-filled-1"  className="task-text" id={task.completed ? "completed" : ""}>
+                                            {task.text}
+                                        </label>
+                                    </div>  
                                 
-                                <div className="checkbox-wrapper-1">
-                                    <input id="checkbox-filled-1" className="checkbox-1" type="checkbox" checked={task.completed} onChange={() => toggleCompleted(task.id)}/>
-                                    <label htmlFor="checkbox-filled-1"  className="task-text" id={task.completed ? "completed" : ""}>
-                                        {task.text}
-                                    </label>
-                                </div>  
-                            
-                                <div className='button-container'>
-                                    <span className='btn-update' onClick={() => deleteTask(task.id)}><i class="fa-solid fa-trash-can"></i></span>
-                                    <span className='btn-update' onClick={() => editTask(task)}><i class="fa-solid fa-pen"></i></span>
-                                </div>
-                                
-                            </div>     
-                        )) 
-                    }
+                                    <div className='button-container'>
+                                        <span className='btn-update' onClick={() => deleteTask(task.id)}><i class="fa-solid fa-trash-can"></i></span>
+                                        <span className='btn-update' onClick={() => editTask(task)}><i class="fa-solid fa-pen"></i></span>
+                                    </div>
+                                    
+                                </div>     
+                            )) 
+                        }
+                    </div>
                 </div>
             </div>
             
