9931a82f7fe94d724b5ccbfca8551b4c003f3b54
imenebaraitame <baraitame.imene@gmail.com>
2025-12-25 16:39:23 +0100

    cleanup: remove unused comments

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,21 +1,9 @@
 import List from './components/list';
-// import { useState } from 'react';
 
-function App() {
-  // const [darkMode, setDarkMode] = useState(false);
-  //  const toggleMode = () => {
-  //       setDarkMode(prev => !prev);
-  //   };
-  
+function App() {  
   return (
     <>
-      {/* <div className={darkMode ? "dark" : "light"}>
-          <button className="toggle-btn" onClick={toggleMode}>
-              <i className={darkMode ? "fa-solid fa-sun" : "fa-solid fa-moon"}></i>
-          </button> */}
-      
         <List />  
-      {/* </div>  */}
     </>
   );
 }
